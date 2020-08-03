78713b36c9e5c799fa2797fdbe75b2b3efacea08
Jacob Daughtry <akutozo@gmail.com>
2020-08-02 21:32:38 -0500

    Added array for questions

--- index.js
+++ index.js
@@ -5,9 +5,67 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-    
-];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter your project title.',
+            validate: titleName => {
+                if (titleName) {
+                    return true;
+                } else {
+                console.log('You must enter a title for your project.');
+                return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'toc',
+            message: 'Enter your Table of Contents',
+            validate: tocInput => {
+                if (tocInput) {
+                    return true;
+                } else {
+                    console.log('A Table of Contents is required.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'Installation Instructions',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Proper Usage',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Detail contributers to this project, if any.',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'Provide any relevant testing information.',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Additional Questions',
+            default: false
+        }
+        
+    ]);
+};
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -19,4 +77,4 @@ function init() {
 }
 
 // function call to initialize program
-init();
+questions()
